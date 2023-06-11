317a235f29dba3b244c28ba4f18dc84d56e45dbe
NasifAhmed99 <98277173+NasifAhmed99@users.noreply.github.com>
2023-06-11 22:15:59 +0600

    Company Page

---
+++ LabTask-1/LabTask-1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabTask_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Homepage()
+        {
+            ViewBag.Name = "NasifAhmed Inc.";
+            ViewBag.Bio = "A leading provider of IT software solutions. We offer a wide range of products and services, including software development, consulting, and training. Our team of experienced professionals is dedicated to providing our customers with the best possible solutions.";
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+      
+            return View();
+        }
+
+        public ActionResult Teams_and_members()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
